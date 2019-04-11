eca1076fda1a1424c321b3a08e84514717abf165
mzhengah <mzhengah@csl4wk64.win2k.cs.ust.hk>
2019-04-11 08:35:09 +0800

    I delete there

--- lab9/src/main/java/Library.java
+++ lab9/src/main/java/Library.java
@@ -3,7 +3,7 @@
  */
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("there is text");
+		System.out.println(" is text");
 	}
     public boolean someLibraryMethod() {
         return true;
